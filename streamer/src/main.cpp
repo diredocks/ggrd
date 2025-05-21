1392a363620cf9ae288c1f0a643aac1153825293
diredocks <26994007+diredocks@users.noreply.github.com>
2025-05-21 20:21:20 +0800

    streamer: add logo

--- streamer/src/main.cpp
+++ streamer/src/main.cpp
@@ -2,8 +2,17 @@
 #include "VideoStreamer.hpp"
 #include "WebSocketServer.hpp"
 #include "spdlog/spdlog.h"
+#include <iostream>
 
 int main() {
+  std::cout << "                         .___" << std::endl;
+  std::cout << "   ____   ___________  __| _/" << std::endl;
+  std::cout << "  / ___\\ / ___\\_  __ \\/ __ |" << std::endl;
+  std::cout << " / /_/  > /_/  >  | \\/ /_/ |" << std::endl;
+  std::cout << " \\___  /\\___  /|__|  \\____ |" << std::endl;
+  std::cout << "/_____//_____/            \\/" << std::endl;
+  std::cout << std::endl;
+
   spdlog::set_pattern("[%C-%m-%d %H:%M:%S] [%^%L%$] %v");
 
   WebSocketServer server;
