aadf10ed2d50e9e628d32a183a7ca95e61e4e3ae
diredocks <26994007+diredocks@users.noreply.github.com>
2025-05-21 20:21:35 +0800

    flake.nix: go related dependency

--- flake.nix
+++ flake.nix
@@ -1,24 +1,46 @@
 {
-  description = "Dev environment for face recognition WebSocket project";
+  description = "ggrd, real-time face recognizer and streamer";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        cmake
-        gcc
-        clang-tools
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
 
-        opencv
-        dlib
-        zlib
-        spdlog
-      ];
+    pkgs = forEachSystem (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [self.overlays.default];
+      });
+  in {
+    formatter = forEachSystem (system: pkgs.${system}.alejandra);
+
+    overlays.default = final: prev: {
+      go = prev.go_1_24;
     };
+
+    devShells = forEachSystem (system: {
+      default = pkgs.${system}.mkShell {
+        packages = with pkgs.${system}; [
+          # build and dev tools of streamer
+          cmake
+          gcc
+          clang-tools
+          # dependency of building streamer
+          opencv
+          dlib
+          zlib
+          spdlog
+          # http server
+          go
+          gotools
+          golangci-lint
+          gopls
+        ];
+      };
+    });
   };
 }
